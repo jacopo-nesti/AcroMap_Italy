35b46e9fabbf4463e34855abd823c9e8348845ee
Jacopo Nesti <jacopo.nesti95@gmail.com>
2026-08-17 09:57:45 +0200

    fix: resolve Leaflet marker icons in production

--- src/components/CityMarker.jsx
+++ src/components/CityMarker.jsx
@@ -1,13 +1,29 @@
 import { Marker, Popup } from "react-leaflet"
 import { Link } from "react-router"
+import L from "leaflet"
+
+import markerIcon from "leaflet/dist/images/marker-icon.png"
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png"
+import markerShadow from "leaflet/dist/images/marker-shadow.png"
+
+const defaultIcon = L.icon({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+})
 
 function CityMarker({ city }) {
 
     return (
-        <Marker position={[city.coordinates.lat, city.coordinates.lng]}>
-
+        <Marker
+            position={[city.coordinates.lat, city.coordinates.lng]}
+            icon={defaultIcon}
+        >
             <Popup>
-
                 <h3>{city.name}</h3>
 
                 <p>{city.region}</p>
@@ -15,12 +31,9 @@ function CityMarker({ city }) {
                 <Link to={`/city/${city.slug}`}>
                     Vai alla community
                 </Link>
-
             </Popup>
-
         </Marker>
     )
-
 }
 
 export default CityMarker
